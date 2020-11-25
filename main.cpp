d87392f69706c7c52a8455d5a8392e755fc35bdb
Varad Deouskar <varadd@gmail.com>
2020-11-26 00:08:40 +0530

    Made Camera Movable

--- main.cpp
+++ main.cpp
@@ -60,7 +60,7 @@ int main()
     world.add(std::make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 	
 	//Camera
-	camera cam;
+	camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0),90,aspect_ratio);
 
 
     // Render
